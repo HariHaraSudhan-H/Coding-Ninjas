5d0a235cb06cd9c9cf358ad2b8c2e8ac6e0cff48
HariHaraSudhan H <hari.sudhan612@gmail.com>
2022-12-19 22:32:25 +0530

    Update stacksUse.java

--- Stacks/stacksUse.java
+++ Stacks/stacksUse.java
@@ -1,5 +1,9 @@
+import java.util.Stack;
+
 public class stacksUse{
     public static void main(String[] args) throws stackFullException, stackEmptyException {
+        Stack<Integer> stack1 = new Stack<>();
+        stack1.peek();
         stacksByArray stack = new stacksByArray();
         stack.push(3);
         stack.push(5);
@@ -10,6 +14,15 @@ public class stacksUse{
         stack.push(6);
         System.out.println(stack.pop());
         System.out.println(stack.isEmpty());
-
+        stacksByLL stacksByLL = new stacksByLL();
+        stacksByLL.push(3);
+        stacksByLL.push(5);
+        //System.out.println(stack.size());
+        System.out.println(stacksByLL.pop());
+        //System.out.println(stack.size());
+        System.out.println(stacksByLL.pop());
+        stacksByLL.push(6);
+        System.out.println(stacksByLL.pop());
+        System.out.println(stacksByLL.isEmpty());
     }
 }
