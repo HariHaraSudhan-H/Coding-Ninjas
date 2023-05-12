5a4ff9c92b061eec2055c52da3050bca079c5c17
HariHaraSudhan H <hari.sudhan612@gmail.com>
2023-05-12 22:47:16 +0530

    update

--- Backend/Codial/controllers/userController.js
+++ Backend/Codial/controllers/userController.js
@@ -1,5 +1,10 @@
 module.exports.userProfile = function(req,res){
-    return res.end('<h1>User Profile</h1>')
+    return res.render('user',{
+        title: 'Codial',
+        user:{
+            name:'Hari'
+        }
+    });
 }
 // module.exports.newPost = function(req,res){
 //     return res.end('<h1>Hello World!</h1>');
--- Backend/Codial/index.js
+++ Backend/Codial/index.js
@@ -7,7 +7,7 @@ app.set('view engine','ejs');
 app.set('views','./views')
 // using express router
 
-app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : $err`)
