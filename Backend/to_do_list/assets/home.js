ca194be915ac191aba46f359a54bb881fd000d81
HariHaraSudhan H <hari.sudhan612@gmail.com>
2023-05-15 06:59:15 +0530

    Formatting Done for Codes

--- Backend/to_do_list/assets/home.js
+++ Backend/to_do_list/assets/home.js
@@ -1,27 +1,29 @@
 console.log('connected to js')
 let category = document.querySelectorAll('.outputCategory');
+
 // let confirmation = window.confirm("hello");
 console.log(category);
+
 //Changing the background color of category accordingly
-for(let i of category){
+for (let i of category) {
     console.log(i.innerText);
-    if(i.innerText=="CHOOSE ONE OPTION"){
+    if (i.innerText == "CHOOSE ONE OPTION") {
         // i.style.display='none';
-        i.style.backgroundColor ='inherit';
+        i.style.backgroundColor = 'inherit';
         i.style.color = 'black'
     }
-    if(i.innerText=="WORK"){
+    if (i.innerText == "WORK") {
         i.style.backgroundColor = 'orange';
     }
 }
 //Toggling the task as completed and not completed
 let done = document.querySelectorAll('.done');
 console.log(done);
-for(let i of done){
+for (let i of done) {
     console.log(i.dataset);
-    if(i.dataset.done=='true'){
-        document.getElementById(i.id).style.textDecoration='line-through';
-        document.getElementById(i.id).style.textDecorationColor='red';
+    if (i.dataset.done == 'true') {
+        document.getElementById(i.id).style.textDecoration = 'line-through';
+        document.getElementById(i.id).style.textDecorationColor = 'red';
 
     }
 }
